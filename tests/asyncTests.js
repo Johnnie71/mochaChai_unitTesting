67b9821ca80115df83893723361544ab32c2d382
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 10:57:45 -0400

    mocha done call

--- tests/asyncTests.js
+++ tests/asyncTests.js
@@ -6,8 +6,9 @@ function myAsyncFunction(callback){
     }, 50);
 };
 
-it('test_async', function(){
+it('test_async', function(done){
     myAsyncFunction(function(str){
         expect(str).to.equal('doh');
     });
+    done();
 });
